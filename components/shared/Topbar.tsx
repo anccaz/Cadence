ba76505e29263c00318abce5c3ed1236ba78cc24
Annie Li <108150567+anccaz@users.noreply.github.com>
2025-04-07 13:50:17 -0500

    clerk themes added, logo added, and cadence title added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
-  return <h1>Topbar</h1>
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flexiterms-center gap-4">
+        <Image src="/logo.png" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Cadence
+
+        </p>
+      </Link>
+    </nav>
+  )
 }
 
 export default Topbar;
