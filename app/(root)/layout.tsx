58ce47d0ea70a41b70902e2abcf3569d6ee7e6d6
WD40Drinker <Mitchtvu@hotmail.com>
2025-04-09 13:21:30 -0500

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-//import { dark } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -13,7 +13,7 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
+  title: "Cadence",
   description: "A Next.js 13 Meta Threads application",
 };
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        //baseTheme: dark,
+        baseTheme: dark,
       }}
     >
       <html lang='en'>
